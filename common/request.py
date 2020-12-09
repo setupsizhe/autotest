a2383beedc88077ce45fb87d740a8844e0794e67
liusizhe <sizhe.liu@mushiny.com>
2020-12-09 14:34:04 +0800

    uhui

--- common/request.py
+++ common/request.py
@@ -1,11 +1,9 @@
 # coding:utf-8
-import os
 import requests
-import sys
-import yaml
+import os
+from duqu.readexcel import ExcelUtil
 
-
-def send_requests(s, testdata):
+def send_requests(testdata):
     '''封装requests请求'''
     method = testdata["method"]
     url = testdata["url"]
@@ -15,7 +13,7 @@ def send_requests(s, testdata):
     except:
         params = None
 # 请求头部headers
-    header=None
+    header=eval(testdata["headers"])
 
     test_nub = testdata['ID']
     print("*******正在执行用例：-----  %s  ----**********" % test_nub)
@@ -24,69 +22,59 @@ def send_requests(s, testdata):
     print("请求headers: %s"%header)
 
 
-    try:
-        bodydata = testdata["body"]
-    except:
-        bodydata = {}
+    # try:
+    #     bodydata = testdata["body"]
+    # except:
+    #     bodydata = {}
+    #
+    # # 判断传data数据还是json
+    # if type == "data":
+    #     body = bodydata
+    # elif type == "json":
+    #     body = json.dumps(bodydata)
+    # else:
+    #     body = bodydata
+    # if method == "post": print("请求body类型为：post" )
 
-    # 判断传data数据还是json
-    if type == "data":
-        body = bodydata
-    elif type == "json":
-        body = json.dumps(bodydata)
-    else:
-        body = bodydata
-    if method == "post": print("请求body类型为：post" )
-
-    verify = False
+    # verify = False
     res = {}   # 接受返回数据
+    # if method=='GET'
 
-    try:
-        r = s.request(method=method,
-                      url=url,
-                      params=params,
-                      headers=header,
-                      verify=verify
-                       )
-        print("页面返回信息：%s" % r.content.decode("utf-8"))
-        res['id'] = testdata['ID']
-        res['rowNum'] = testdata['rowNum']
+    r = requests.request(method=method,
+                  url=url,
+                  params=params,
+                  headers=header
+                   )
+    print("页面返回信息：%s" % r.content.decode("utf-8"))
+    res['id'] = testdata['ID']
+    res['rowNum'] = testdata['rowNum']
 
 
-        res["statuscode"] = str(r.status_code)  # 状态码转成str
-        res["text"] = r.content.decode("utf-8")
+    res["statuscode"] = str(r.status_code)  # 状态码转成str
+    res["text"] = r.content.decode("utf-8")
+    # print(res["text"])
+    res["times"] = str(r.elapsed.total_seconds())
+    # 接口请求时间转str
+    if res["statuscode"] != "200":
+        res["error"] = res["text"]
+        print(res["text"])
+    else:
+        res["error"] = ""
         # print(res["text"])
-        res["times"] = str(r.elapsed.total_seconds())
-        # 接口请求时间转str
-        if res["statuscode"] != "200":
-            res["error"] = res["text"]
-            print(res["text"])
-        else:
-            res["error"] = ""
-            # print(res["text"])
-        res["msg"] = ""
-        if testdata["checkpoint"] in res["text"]:
-            res["result"] = "pass"
-            print("用例测试结果:   %s---->%s" % (test_nub, res["result"])+'\n\n')
-        else:
-            res["result"] = "fail"
-        return res
-    except Exception as msg:
-        res["msg"] = str(msg)
-        return res
-
+    res["msg"] = ""
+    if testdata["checkpoint"] in res["text"]:
+        res["result"] = "pass"
+        print("用例测试结果:   %s---->%s" % (test_nub, res["result"])+'\n\n')
+    else:
+        res["result"] = "fail"
+        print("用例测试结果:   %s---->%s" % (test_nub, res["result"]) + '\n\n')
+    return res
 
 if __name__ == "__main__":
-    cur_path=os.path.dirname(os.path.realpath(__file__))
-    yaml_path=os.path.join(cur_path,'D://untitled6//conf//canshu')
-    datas = open(yaml_path,'r',encoding='UTF-8')
-    testdatas = yaml.load(datas, Loader=yaml.FullLoader)
-    data =testdatas
-    s = requests.session()
-    res = send_requests(s, data[0])
-    # print(data)
-    # print(data[0]['params'])
-    # print(s.headers)
+    data = ExcelUtil(excelPath='test.xlsx', sheetName="Sheet1").dict_data()
+    send_requests(data[0])
+    print(os.path.abspath(os.path.realpath(__file__)))
+
 
 
 
---
+++ test/test_login.py
@@ -0,0 +1,38 @@
+import pytest
+import requests
+import json
+import yaml
+import os
+import allure_pytest
+import allure
+import allure_commons
+from common import request
+from duqu.readexcel import ExcelUtil
+testdata2= ExcelUtil(excelPath='test.xlsx', sheetName="Sheet1").dict_data()
+
+@allure.story("登录用例")
+@pytest.mark.parametrize('url',testdata2)
+
+def test_dic(url):
+    res = request.send_requests(url)
+    res_text=res['statuscode']
+    print(res_text)
+    # print("检查点->：%s"%check)
+    check=url['checkpoint']
+    print(check)
+    # print("返回实际结果->：%s"%res_text)
+    # for i in res:
+    #     print(res['times'])
+
+    assert check != res_text
+
+# def test_three(token):
+#
+#     r = requests.get(url='http://221.179.136.72:33200/EPG/interEpg/user/default/authorize',
+#                   headers={'Authorization': token}
+#                    )
+#     print(r.json())
+#     assert (r.status_code==20
+if __name__=='__main__':
+    pytest.main(['-s ','test_login.py'])
+
